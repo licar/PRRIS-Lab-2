710e5d5e0fd397137b297717adb286243fa99b76
evgeny-polikarpov <epo@porita.ru>
2018-12-10 17:42:55 +0300

    Init

---
+++ Notebook/Node.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace Notebook
+{
+	public class NodeTrie
+	{
+		private readonly Dictionary<char, NodeTrie> childs;
+
+		protected int Count { get; set; }
+		protected bool Word { get; set; }
+
+		public NodeTrie()
+		{
+			childs = new Dictionary<char, NodeTrie>();
+			Count = 0;
+			Word = false;
+		}
+
+		public bool AddChild(string str)
+		{
+			var ch = str[0];
+
+			if (!childs.TryGetValue(ch, out var child))
+			{
+				child = new NodeTrie();
+				childs.Add(ch, child);
+				if (str.Length == 1)
+				{
+					++child.Count;
+					child.Word = true;
+					return true;
+				}
+			}
+			else if (str.Length == 1)
+			{
+				if (!child.Word)
+				{
+					++child.Count;
+					child.Word = true;
+					return true;
+				}
+				return false;
+			}
+
+			if (child.AddChild(str.Substring(1, str.Length - 1)))
+			{
+				++child.Count;
+				return true;
+			}
+
+			return false;
+		}
+
+		public int Search(string str)
+		{
+			var ch = str[0];
+
+			if (!childs.TryGetValue(ch, out var child))
+			{
+				return 0;
+			}
+
+			return str.Length == 1 ? child.Count : child.Search(str.Substring(1, str.Length - 1));
+		}
+	}
+}
---
+++ Notebook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Notebook
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var notebook = new NodeTrie();
+
+			Console.WriteLine($"Enter 's' for start search");
+			while (true)
+			{
+				var input = Console.ReadLine();
+				if (input.Equals("s", StringComparison.OrdinalIgnoreCase))
+				{
+					break;
+				}
+
+				if (notebook.AddChild(input))
+				{
+					Console.WriteLine($"Add {input}");
+				}
+				else
+				{
+					Console.WriteLine($"Alread exists {input}");
+				}
+				
+			}
+
+			Console.WriteLine($"Enter 'e' for start search");
+			while (true)
+			{
+				var input = Console.ReadLine();
+				if (input.Equals("e", StringComparison.OrdinalIgnoreCase))
+				{
+					break;
+				}
+
+				var count = notebook.Search(input);
+				Console.WriteLine($"{input} -> {count}");
+			}
+		}
+	}
+}
